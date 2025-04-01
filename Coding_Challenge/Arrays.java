e6d9c7d9d0363837f388087c084e3700feff8adb
ErbinJames <eorodio04922@student.dmmmsu.edu.ph>
2025-04-01 23:11:08 +0800

    pushing first coding challenge

--- Coding_Challenge/Arrays.java
+++ Coding_Challenge/Arrays.java
@@ -1,1 +1,17 @@
+package Coding_Challenge;
 
+public class Arrays {
+    public static void main (String[] args) {
+        // initializing array of numbers
+        int [] numbers = {5, 10, 15, 20, 25};
+        int sum = 0;
+        
+        //using an enhanced for loop for every elements in the numbers array
+        for (int num : numbers) {
+            sum += num;
+        }
+        
+        //display the output
+        System.out.println("Sum of elements: " + sum);
+    }
+}
